84a3e3937a385b0666a8cd8fa5c11382ac43bc79
sharwinxavier <65103675+sharwinxavier@users.noreply.github.com>
2023-03-18 09:08:39 +0530

    march 18

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pyrebase
 import logging
 import sys
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from flask import render_template
+from flask import make_response
+import pdfkit
+import random
 
 app = Flask(__name__)       #Initialze flask constructor
 
@@ -19,7 +23,11 @@ auth = firebase.auth()
 db = firebase.database()
 
 #Initialze person as dictionary
-person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
+person = {"is_logged_in": False, "name": "", "email": "", "uid": "", "vsub": "", "vsubname": ""}
+person1 = {"is_logged_in": False, "name": "", "email": "", "uid": "", "vsub": "", "vsubname" : ""}
+person2 = {"is_logged_in": False, "name": "", "email": "", "uid": "", "vsub": "", "vsubname" : ""}
+
+config1 = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
 
 #Login
 @app.route("/")
@@ -78,7 +86,19 @@ def newsub():
             db.child("subjects").child(str(len(sub_list_1.val()))).set(subject_details)
             validator_details = {"validator email": validator_email, "validator password": validator_password, "validator name": validator_name, "validator subject": subject_code, "validator subject name": subject_name}
             val_list_1 = db.child('validators').get()
-            db.child("validators").child(str(len(val_list_1.val()))).set(validator_details)
+            auth.create_user_with_email_and_password(validator_email, validator_password)
+            global person1
+            # person1["is_logged_in"] = True
+            person1["email"] = validator_email
+            person1["uid"] = random.randint(1,100000000)
+            person1["name"] = validator_name
+            person1["vsub"] = subject_code
+            person["vsubname"] = subject_name
+            #Append data to the firebase realtime database
+            data = {"validator name": validator_name, "validator email": validator_email, "validator password": validator_password, "validator subject":subject_code, "validator subject name":subject_name}
+            # if(name=="admin" and email=="admin@qgen.com"):   #password = admin@qgen
+            db.child("validators").child(person1["uid"]).set(data)
+            # db.child("validators").child(str(len(val_list_1.val()))).set(validator_details)
             return redirect(url_for('dashboard'))
         except:
             return redirect(url_for('login'))
@@ -128,6 +148,27 @@ def downroc1():
         return render_template("downroc1.html", email=person["email"], name = person["name"], specs=res2)
     else:
         return redirect(url_for('login'))
+    
+@app.route("/downproc", methods=["POST","GET"])
+def downproc():
+    if person["is_logged_in"] == True:
+        if request.method == "POST":
+            res1 = list(request.form.listvalues())
+            res2=[]
+            for i in res1:
+                res2.extend(i)
+            print(res2)
+        name = "Sharwin Xavier R"
+        html = render_template(
+            "certificate.html",
+            name=name)
+        pdf = pdfkit.from_string(html, configuration=config1)
+        response = make_response(pdf)
+        response.headers["Content-Type"] = "application/pdf"
+        response.headers["Content-Disposition"] = "inline; filename=output.pdf"
+        return response
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/uproc", methods=["POST","GET"])
 def uproc():
@@ -144,6 +185,7 @@ def uproc():
                 question_details = {"question":res2[i],"mark":res2[i+1],"co_level":res2[i+2],"ko_level":res2[i+3],"difflev":res2[i+4]}
                 temp = db.child("questions").child(a).get()
                 print(temp.val())
+                
                 if(temp.val() is not None):
                     db.child("questions").child(a).child(str(len(temp.val()))).set(question_details)
                 else:
@@ -173,6 +215,10 @@ def result():
                 data=db.child("admin").get()
                 person["name"] = data.val()[person["uid"]]["name"]
                 return redirect(url_for('dashboard'))
+            elif(person["email"][:3]=="val"):
+                data = db.child("users").get()
+                person["name"] = data.val()[person["uid"]]["name"]
+                return redirect(url_for('vallist'))
             else:
                 data = db.child("users").get()
                 person["name"] = data.val()[person["uid"]]["name"]
@@ -201,7 +247,7 @@ def vresult():
             if email[:3]=='val' and email[-8:]=='qgen.com' and name[:9]=='Validator':
                 # print("vallllli")
                 valdata = db.child("validators").order_by_child("validator name").equal_to(name).get()
-                # print(valdata.val())
+                lid = list(valdata.val().items())[0][0]
                 flag=0
                 temp=''
                 temp1=''
@@ -217,7 +263,31 @@ def vresult():
                 # print(temp, temp1)
                 # print(email, password)
                 if flag==1:
-                    return render_template("vallist.html", email=email, password=password, validator_subject = temp, validator_subject_name=temp1)
+                    print("blah")
+                    # page = auth.list_users()
+                    # while page:
+                    #     for user in page.users:
+                    #         print('User: ' + user.uid)
+                    #     # Get next batch of users.
+                    #     page = page.get_next_page()
+
+                    # # Iterate through all users. This will still retrieve users in batches,
+                    # # buffering no more than 1000 users in memory at a time.
+                    # for user in auth.list_users().iterate_all():
+                    #     print('User: ' + user.uid)
+                    user1 = auth.sign_in_with_email_and_password(email, password)
+                    print("blah1")
+                    global person2
+                    person2["is_logged_in"] = True
+                    person2["email"] = user1["email"]
+                    person2["uid"] = lid
+                    person2["name"] = name
+                    person2["vsub"] = temp
+                    person2["vsubname"] = temp1
+                    print("blah2")
+                    return redirect(url_for('vallist'))
+                    # return render_template("vallist.html", email=email, password=password, validator_subject = temp, validator_subject_name=temp1, uid1=lid)
+                    # return render_template("vallist.html")
                 # print("jojo")
                 # return 'hi'
             #Try signing in the user with the given information
@@ -244,6 +314,15 @@ def vresult():
             return redirect(url_for('welcome'))
         else:
             return redirect(url_for('login'))
+        
+@app.route("/vallist")
+def vallist():
+    if person2["is_logged_in"] == True:
+        return render_template("vallist.html", email = person2["email"], name = person2["name"], validator_subject = person2["vsub"], validator_subject_name = person2["vsubname"])
+        # return render_template("vallist.html", email = "blah!")
+        # return 'hi'
+    else:
+        return redirect(url_for('login'))
 
 #If someone clicks on register, they are redirected to /register
 @app.route("/register", methods = ["POST", "GET"])
@@ -264,11 +343,26 @@ def register():
             person["email"] = user["email"]
             person["uid"] = user["localId"]
             person["name"] = name
+            # person["vsub"] = ""
             #Append data to the firebase realtime database
             data = {"name": name, "email": email}
+            
             # if(name=="admin" and email=="admin@qgen.com"):   #password = admin@qgen
             db.child("users").child(person["uid"]).set(data)
-            return redirect(url_for('welcome'))
+            
+            if(email[:3]=='val'):
+                
+                # print(vsub,vsubname,person["email"])
+                valdata = {"validator name": name, "validator email": email, "validator subject": "ahh", "validator subject name": ""}
+                db.child("validators").child(person["uid"]).set(valdata)
+                lido = db.child("validators").child(person["uid"]).get()
+                vsub = list(lido.val().items())[2][1]
+                vsubname = list(lido.val().items())[3][1]
+                person["vsub"] = vsub
+                person["vsubname"] = vsubname
+                return redirect(url_for('vallist'))
+            else:
+                return redirect(url_for('welcome'))
             # else:
             #     db.child("users").child(person["uid"]).set(data)
             # #Go to welcome page
